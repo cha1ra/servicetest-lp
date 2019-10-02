3e6d9eded71bab53e2869d015df47ba211279fd7
cha1ra <cha1ra.garnet@gmail.com>
2019-10-02 17:49:11 +0900

    change description

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '貴方の夢、叶えます。' }
+      { hid: 'description', name: 'description', content: '「時間がないから...」「自分のアイディアに自身がなくて...」そんなあなたの夢、サポートします。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
